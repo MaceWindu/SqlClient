865faa172a930b2c870d6de8237aa741f3fac4f9
Paul Medynski <31868385+paulmedynski@users.noreply.github.com>
2025-01-09 12:58:08 -0400

    Task 33292: Add tests to Cheena's work
    - Moved logic to build client interface name into its own ClientInterface class.
    - Added tests for ClientInterface.

---
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/ClientInterface.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Microsoft.Data.SqlClient
+{
+    // ========================================================================
+    // This class uses runtime environment information to produce a value
+    // suitable for use in the TDS LOGIN7 Client Interface Name field.
+    //
+    internal static class ClientInterface
+    {
+        // ====================================================================
+        // Properties
+
+        // --------------------------------------------------------------------
+        // The client interface name, never null, never empty, and never larger
+        // than 128 characters.
+        //
+        // Format:
+        //
+        //   Microsoft SqlClient - {OS Name} {OS Version}, {Runtime} - {Arch}
+        //
+        // The {OS Name} will be one of the following strings:
+        //
+        //   Windows
+        //   Linux
+        //   macOS
+        //   FreeBSD
+        //   Unknown
+        //
+        // The {OS Version} will be whatever the value of
+        // System.Environment.OSVersion.Version is.
+        //
+        // The {Runtime} will be the value of
+        // System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.
+        //
+        // The {Arch} will be the value of
+        // System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.
+        //
+        // This adheres to the TDS v37.0 spec, which specifies that the client
+        // interface name has a maximum length of 128 unicode characters.  If
+        // the format above expands beyond that limit, it will be truncated.
+        //
+        public static string Name => _name;
+
+        // ====================================================================
+        // Private Helpers
+
+        // --------------------------------------------------------------------
+        // Static construction builds the client interface name.
+        //
+        // All known exceptions are caught and handled by providing a fallback
+        // value.  However, no effort is made to catch all exceptions.
+        //
+        static ClientInterface()
+        {
+            try
+            {
+                // Expect to build a string whose length is up to our max
+                // length.
+                //
+                // This isn't a max capacity, but a hint for initial buffer
+                // allocation.  We will truncate to our max length after all of
+                // the pieces have been appended.
+                StringBuilder name = new(TdsEnums.MAXLEN_CLIENTINTERFACE);
+
+                // Start with the well-known name of this driver.
+                name.Append(Common.DbConnectionStringDefaults.ApplicationName);
+                name.Append(" - ");
+
+                // Add the OS name, in order of likelihood.
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    name.Append("Windows");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    name.Append("Linux");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    name.Append("macOS");
+                }
+#if NET
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD))
+                {
+                    name.Append("FreeBSD");
+                }
+#endif // NET
+                else
+                {
+                    name.Append(_unknown);
+                }
+                name.Append(' ');
+
+                try
+                {
+                    // Add the OS version.
+                    //
+                    // Per the System.Version.ToString() documentation, this
+                    // takes the form:
+                    //
+                    //   {Major}.{Minor}[.{Build}[.{Revision}]]
+                    //
+                    // Where [] indicate an optional part.
+                    //
+                    // All parts are decimal integers.
+                    //
+                    name.Append(Environment.OSVersion.Version);
+                }
+                catch (InvalidOperationException)
+                {
+                    // Environment.OSVersion failed in an unexpected way, so use
+                    // a fallback value.
+                    name.Append(_unknown);
+                }
+                name.Append(", ");
+
+                // Add the .NET runtime info.
+                //
+                // The documentation for FrameworkDescription doesn't specify
+                // that it will always return a non-null value, so apply our
+                // unknown value in that unlikely case.
+                name.Append(
+                    RuntimeInformation.FrameworkDescription ?? _unknown);
+                name.Append(" - ");
+
+                // Add the architecture.
+                name.Append(RuntimeInformation.ProcessArchitecture);
+
+                // Remember it!
+                _name = name.ToString();
+
+                // Truncate to our max length.
+                if (_name.Length > TdsEnums.MAXLEN_CLIENTINTERFACE)
+                {
+                    _name = _name.Substring(0, TdsEnums.MAXLEN_CLIENTINTERFACE);
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // StringBuilder failed in an unexpected way, so use a fallback
+                // value.
+                _name =
+                  Common.DbConnectionStringDefaults.ApplicationName +
+                  " - " +
+                  _unknown;
+            }
+        }
+
+        // ====================================================================
+        // Private Fields
+
+        // The client interface name.
+        private static readonly string _name;
+
+        // A placeholder string for parts of the client interface name that are
+        // unknown.
+        private const string _unknown = "Unknown";
+    }
+}
--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParser.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/TdsParser.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Buffers;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Text;
 
 #nullable enable
 
@@ -185,7 +183,11 @@ namespace Microsoft.Data.SqlClient
             // length in bytes
             int length = TdsEnums.SQL2005_LOG_REC_FIXED_LEN;
 
-            Debug.Assert(TdsEnums.MAXLEN_CLIENTINTERFACE >= s_clientInterfaceName.Length, "cchCltIntName can specify at most 128 unicode characters. See Tds spec");
+            // Obtain the client interface name.
+            // e.g. "Microsoft SqlClient - Microsoft Windows 10.0.26100, .NET 8.0.11 - X64"
+            string clientInterfaceName = ClientInterface.Name;
+
+            Debug.Assert(TdsEnums.MAXLEN_CLIENTINTERFACE >= clientInterfaceName.Length, "cchCltIntName can specify at most 128 unicode characters. See Tds spec");
 
             // add up variable-len portions (multiply by 2 for byte len of char strings)
             checked
